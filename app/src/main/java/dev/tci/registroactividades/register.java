9ef8930ef4b0e7bcea46a58904d88d7cff4165b1
GeorgeGoAy <32154680+GeorgeGoAy@users.noreply.github.com>
2018-12-20 10:04:03 -0600

    Implementacion de la camara

--- app/src/main/java/dev/tci/registroactividades/register.java
+++ app/src/main/java/dev/tci/registroactividades/register.java
@@ -1,11 +1,21 @@
 package dev.tci.registroactividades;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayout;
 import android.support.v7.widget.Toolbar;
@@ -15,6 +25,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
@@ -22,6 +33,8 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.io.File;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
@@ -33,9 +46,10 @@ import dev.tci.registroactividades.Singleton.Principal;
 
 public class register extends AppCompatActivity {
 
-    private LinearLayout danoLaytou;
+    private LinearLayout danoLaytou,lyPhoto;
     private EditText huerta, productor, telefono, toneladas_aprox, cal32, cal36, cal40, cal48, cal60, cal70, cal84, cal96, calCAN, calLAC,
             danoRONA, danoROSADO, danoBANO, danoTRIPS, danoQUEMADO, danoCOMEDOR, danoVIRUELA, danoVARICELA;
+    private ImageView imgPhoto;
     private int sumaCalibres = 0;
     SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
     Date date = new Date();
@@ -52,6 +66,7 @@ public class register extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         Init();
+        lyPhoto.setVisibility(View.GONE);
         int record = getIntent().getExtras().getInt("record");
 
         calLAC.addTextChangedListener(new TextWatcher() {
@@ -110,6 +125,10 @@ public class register extends AppCompatActivity {
 
                 }
             break;
+
+            case R.id.camera:
+                dispatchTakePictureIntent();
+                break;
         }
         return super.onOptionsItemSelected(item);
     }
@@ -139,6 +158,8 @@ public class register extends AppCompatActivity {
         danoVARICELA = findViewById(R.id.txtDanoVaricela);
         danoLaytou = findViewById(R.id.layout_danos);
         UID = getIntent().getExtras().getString("UID");
+        imgPhoto = findViewById(R.id.imgPhoto);
+        lyPhoto = findViewById(R.id.lyPhoto);
     }
 
     public void guardar(){
@@ -184,6 +205,58 @@ public class register extends AppCompatActivity {
         if(toneladas_aprox.getText().toString().isEmpty()){toneladas_aprox.setError("Es requerido");}
 
     }
+
+    String mCurrentPhotoPath;
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    static final int REQUEST_TAKE_PHOTO = 1;
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "dev.tci.registroactividades",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+    }
+    Bitmap imageBitmap;
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK) {
+            lyPhoto.setVisibility(View.VISIBLE);
+            imageBitmap = BitmapFactory.decodeFile(mCurrentPhotoPath);
+            imgPhoto.setImageBitmap(imageBitmap);
+        }
+    }
+
+
 
 
 }
